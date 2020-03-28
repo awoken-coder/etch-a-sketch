1b20e6428b153928a11e3eaf877e80e543ed3fa6
awoken-coder <jfafandrade@gmail.com>
2020-03-28 17:05:30 +0000

    changed the color of the grid cells to a random color. when the cursor passes a second time over the cell it changes to another random color, until black after 10 times.

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,83 @@
 allTogether(2, 3);
-
 btnClear.addEventListener('click', () => {
     allTogether()
 });
 
+
+function generateArrayColorCode() {
+    arrayColorCode = [];
+    for (i = 0; i < 9; i++) {
+        red = Math.round(Math.random() * 255);
+        blue = Math.round(Math.random() * 255);
+        green = Math.round(Math.random() * 255);
+        arrayColorCode[i] = `rgb(${red}, ${blue}, ${green})`;
+    }
+    arrayColorCode[9] = 'rgb(0,0,0)';
+    return arrayColorCode;
+};
+
+function changeColor(item, color) { //changes the color of a cell
+    if (color[0] == 'white') {
+        item.style.backgroundColor = 'white';
+    }
+    else if (item.style.backgroundColor == 'white') {
+        item.style.backgroundColor = color[0];
+    }
+    else {
+        for (i = 0; i < 10; i++) {
+            if (item.style.backgroundColor == color[i]) {
+                item.style.backgroundColor = color[i + 1];
+                break
+            }
+        }
+
+    };
+    a=typeof item.style.backgroundColor;
+    b=typeof color[0];
+}
+function createGrid(numLin, numCol) { //creates the cell and puts it in the grid
+    let numTotal = numCol * numLin;
+    for (i = 0; i < numTotal; i++) {
+        var square = document.createElement('div');
+        square.classList.toggle('cellCanvas');
+        document.getElementById('containerGrid').appendChild(square);
+    };
+};
+function createContainer(numLin, numCol) { //creates a new container with a grid with x lines and y rows
+    container = document.getElementById('containerGrid');
+    var sizeLin = 1 / numLin * 100;
+    var sizeCol = 1 / numCol * 100;
+    var strLines = `${sizeLin}% `;
+    var strCol = `${sizeCol}% `;
+    strTemplateRows = '';
+    strTemplateCol = '';
+    for (line = 0; line < numLin; line++) {
+        strTemplateRows += strLines;
+    };
+    for (column = 0; column < numCol; column++) {
+        strTemplateCol += strCol;
+    };
+    container.style.gridTemplateColumns = strTemplateCol;
+    container.style.gridTemplateRows = strTemplateRows;
+};
+
 function allTogether() {
+    generateArrayColorCode();
+
     l = prompt('How many lines should the board have?');
     c = prompt('How many rows should the board have?');
-    
-    createContainer(l, c);
-    
-    createGrid(l, c);
-    
-    let cellCanvas = document.querySelectorAll('.cellCanvas');
-    
-    cellCanvas.forEach(item => changeColor(item, 'white'));
 
+    createContainer(l, c);
+
+    createGrid(l, c);
+
+    let cellCanvas = document.querySelectorAll('.cellCanvas');
+    let colorWhite = ['white'];
+    cellCanvas.forEach(item =>{changeColor(item,colorWhite)})
     cellCanvas.forEach(item => {
         item.addEventListener('mouseover', e => {
-            changeColor(e.target, 'black')
+            changeColor(e.target, arrayColorCode)
         })
     });
-
-    function changeColor(item, color) { //changes the color of a cell
-        item.style.backgroundColor = color
-    };
-    function createGrid(numLin, numCol) { //creates the cell and puts it in the grid
-        let numTotal = numCol * numLin;
-        for (i = 0; i < numTotal; i++) {
-            var square = document.createElement('div');
-            square.classList.toggle('cellCanvas');
-            document.getElementById('containerGrid').appendChild(square);
-        };
-    };
-    function createContainer(numLin, numCol) { //creates a new container with a grid with x lines and y rows
-        container = document.getElementById('containerGrid');
-        var sizeLin = 1 / numLin * 100;
-        var sizeCol = 1 / numCol * 100;
-        var strLines = `${sizeLin}% `;
-        var strCol = `${sizeCol}% `;
-        strTemplateRows = '';
-        strTemplateCol = '';
-        for (line = 0; line < numLin; line++) {
-            strTemplateRows += strLines;
-        };
-        for (column = 0; column < numCol; column++) {
-            strTemplateCol += strCol;
-        };
-        container.style.gridTemplateColumns = strTemplateCol;
-        container.style.gridTemplateRows = strTemplateRows;
-    };
+    return arrayColorCode;
 };
